28d06f37af8cdd158d275cac60a65042727b2968
shazzed-76 <shazzed7656@gmail.com>
2026-01-15 01:32:39 +0600

    changed the logo and updated the ui of  about founders section

--- src/components/Netolyze/Netolyze.jsx
+++ src/components/Netolyze/Netolyze.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router';
 import logo from '../../assets/logo/netolyze-logo.png'
-import footerLogo from '../../assets/logo/netolyze-footer-logo.png'
+import footeLogo from '../../assets/logo/footer-netolyze-logo.png'
+import magnifierLogo from "../../assets/logo/magnifier.png";
 const Netolyze = ({isFooter=false}) => {
     return (
-      <Link to={"/"} className="flex items-center">
+      <Link to={"/"} className="flex relative">
         {/* <img src={logo} className='h-10' /> */}
-        <p className="text-white font-bold text-3xl flex">
-          <span
-            className={`-mr-2 ${
-              !isFooter && "gradient_color text-transparent bg-clip-text"
-            }`}
-          >
-            Net
-          </span>
-          {isFooter ? (
-            <img
-              src={footerLogo}
-              className="w-10 mt-1 -rotate-160"
-              alt="netolyze logo"
-            />
-          ) : (
-            <img src={logo} className="w-10 mt-1 -rotate-160" alt="netolyze logo" />
-          )}
+        {isFooter ? (
+          <img src={footeLogo} className="w-10.5" alt="logo" />
+        ) : (
+          <img src={logo} className="w-10" alt="logo" />
+        )}
+
+        <p className="text-white font-bold text-3xl flex absolute top-2 left-10.5">
+          <span className={`-mr-2 ${!isFooter && " bg-clip-text"}`}>et</span>
+          <img
+            src={magnifierLogo}
+            className="w-10 mt-1 -rotate-160"
+            alt="magnifier image"
+          />
           <span
             className={`font-thin text-2xl -ml-2 mt-1 ${
-              !isFooter && "gradient_color text-transparent bg-clip-text"
+              !isFooter && "bg-clip-text"
             }`}
           >
             lyze
--- src/pages/HomePage/AboutFounders/AboutFounders.jsx
+++ src/pages/HomePage/AboutFounders/AboutFounders.jsx
@@ -3,6 +3,7 @@ import Section from "../../../components/Section";
 import Title from "../../../components/Title/Title";
 import mahinChowdhury from '../../../assets/founders-image/mahin-chowdhury.png'
 import sultanMahmud from '../../../assets/founders-image/sultan-mahmud.png'
+import asmaAkter from '../../../assets/founders-image/asma-akter.png'
 import { motion } from "motion/react"
 import TitleAnimation from "../../../components/TitleAnimation/TitleAnimation";
 
@@ -11,8 +12,9 @@ const AboutFounders = () => {
   const data = use(fetchData);
 
   const founders = {
+    asmaAkter,
     mahinChowdhury,
-    sultanMahmud
+    sultanMahmud,
   }
 
   return (
@@ -24,37 +26,40 @@ const AboutFounders = () => {
           customStyle={"text-3xl md:text-5xl text-center mb-20"}
         />
       </TitleAnimation>
-      <div className="flex flex-col md:flex-row gap-5">
+      <div className="grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 gap-5">
         {data.map((singleData, idx) => {
           const foundersImage = founders[singleData?.id];
+          const animationDelay = idx === 0 ? idx : 0.2 ; 
           return (
             <motion.div
               key={idx}
               initial={{ opacity: 0, y: 100 }}
               whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.8, delay: idx }}
-              className="flex-1 flex h-auto xl:h-70  border border-gray-700 xl:border-l-0 xl:border-b-0  xl:rounded-r-xl gap-5  relative group flex-col xl:flex-row overflow-hidden bg-[#140b1e]"
+              transition={{ duration: 0.8, delay: animationDelay }}
+              className={`${idx === 0 && "md:col-span-2 lg:col-span-1 xl:col-span-2"}`}
             >
-              <div className="w-2/5 hidden xl:block absolute bg-gradient-to-b from-[#7B53E8] via-[#5831C3] to-[#0f0717] rounded-r-xl inset-0 group-hover:w-full transition-all duration-1000"></div>
-              {/* profile */}
-              <div className="xl:w-2/5 w-full flex items-center justify-center p-5 bg-gradient-to-t from-[#7B53E8] to-[#5831C3] xl:bg-transparent xl:rounded-b-0 rounded-b-xl py-10 xl:py-0">
-                <div className="avatar">
-                  <div className="w-50 rounded-full ring-black ring-offset-black ring-2 ring-offset-2">
-                    <img src={foundersImage} />
+              <div className="flex-1 flex h-full xl:h-70  border border-gray-700 xl:border-l-0 xl:border-b-0  xl:rounded-r-xl gap-5  relative group flex-col xl:flex-row overflow-hidden bg-[#140b1e] w-full md:max-w-83.5 lg:max-w-none xl:max-w-180 mx-auto">
+                <div className="w-2/5 hidden xl:block absolute bg-linear-to-b from-[#7B53E8] via-[#5831C3] to-[#0f0717] rounded-r-xl inset-0 group-hover:w-full transition-all duration-1000"></div>
+                {/* profile */}
+                <div className="xl:w-2/5 w-full flex items-center justify-center p-5 bg-linear-to-t from-[#7B53E8] to-[#5831C3] xl:bg-transparent xl:rounded-b-0 rounded-b-xl py-10 xl:py-0">
+                  <div className="avatar">
+                    <div className="w-50 rounded-full ring-black ring-offset-black ring-2 ring-offset-2">
+                      <img src={foundersImage} />
+                    </div>
                   </div>
                 </div>
-              </div>
-              {/* content */}
-              <div className="xl:w-3/5 w-full flex flex-col justify-center font-semibold relative z-10 p-5">
-                <h3 className="text-xl bg-gradient-to-r from-[#fafafa] to-[#0f0717] text-transparent bg-clip-text">
-                  {singleData?.role}
-                </h3>
-                <h4 className="text-2xl text-[#fafafa] mb-2">
-                  {singleData?.name}
-                </h4>
-                <p className="text-lg custom_font_color">
-                  {singleData?.skills.join(" | ")}
-                </p>
+                {/* content */}
+                <div className="xl:w-3/5 w-full flex flex-col justify-center font-semibold relative z-10 p-5">
+                  <h3 className="text-xl bg-linear-to-r from-[#fafafa] to-[#0f0717] text-transparent bg-clip-text">
+                    {singleData?.role}
+                  </h3>
+                  <h4 className="text-2xl text-[#fafafa] mb-2">
+                    {singleData?.name}
+                  </h4>
+                  <p className="text-lg custom_font_color">
+                    {singleData?.skills.join(" | ")}
+                  </p>
+                </div>
               </div>
             </motion.div>
           );
